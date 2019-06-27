922138ed8f7118d36a8a82c8e8ab3438c20f6605
Jordan Gunther <jordankgunther@gmail.com>
2019-06-27 16:28:14 -0600

    set up router to use history mode and added url to learn how to set up server to work with it

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import { routes } from './routes';
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes //es6 shortcut for routes: routes,
+  routes, //es6 shortcut for routes: routes,
+  mode: 'history' // this removes the /#/ from the url, make sure your server is set up to return the index even in the case of a 404.  https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
 });
 
 new Vue({
