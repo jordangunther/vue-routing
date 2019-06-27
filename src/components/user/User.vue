849d5e48cabcdb293a88f4370e8ba4c200e98086
Jordan Gunther <jordankgunther@gmail.com>
2019-06-27 16:53:00 -0600

    pass a user id in the url and access it

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded ID: {{ id }}</p>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 
@@ -9,6 +10,11 @@
 
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
         methods: {
             navigateToHome() {
                 this.$router.push('/');
