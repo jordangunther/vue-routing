f307da810b11b3dafc54fcd60b35ed81971706b2
Jordan Gunther <jordankgunther@gmail.com>
2019-06-28 11:50:57 -0600

    set up anchor scrolling and set up a confirm button on the edit page to ask the user if they are sure they want to leave the page

--- src/main.js
+++ src/main.js
@@ -7,7 +7,16 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes, //es6 shortcut for routes: routes,
-  mode: 'history' // this removes the /#/ from the url, make sure your server is set up to return the index even in the case of a 404.  https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
+  mode: 'history', // this removes the /#/ from the url, make sure your server is set up to return the index even in the case of a 404.  https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return { x: 0, y: 0 };
+  }
 });
 
 new Vue({
